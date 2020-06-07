096c728647060d917510946cfca56121dec774b5
Stephen Young <stephen@stephen-young.me.uk>
2020-06-07 22:04:17 +0100

    Updating server

--- src/fpl/server.clj
+++ src/fpl/server.clj
@@ -13,6 +13,6 @@
 (defn handler [request]
   {:status  200
    :headers {"Content-Type" "application/json"}
-   :body    (json/write-str (calculate-expected-values)))
+   :body    (json/write-str (calculate-expected-values fpl-data 38 29 [30 31 32 33 34 35 36 37 38]))})
 
 (defn -main [] (run-jetty handler {:port 3000}))
